78c766e8207dbe8750e92bb916c683fa682898d0
José Lima <josekenosis@gmail.com>
2023-02-09 11:06:18 -0400

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'skepfy',
+      entities: [User],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
+})
+export class AppModule {}
